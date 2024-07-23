a53d9804864ae29d07effd39231a64128f38e074
Vitalie Cherpec <vitalie@penguin.ro>
2024-07-23 16:16:22 +0300

    Include template_id field

--- zone.go
+++ zone.go
@@ -3,9 +3,10 @@ package luadns
 import "time"
 
 type Zone struct {
-	ID        int64     `json:"id,omitempty"`
-	Name      string    `json:"name"`
-	Records   []*Record `json:"records,omitempty"`
-	CreatedAt time.Time `json:"created_at,omitempty"`
-	UpdatedAt time.Time `json:"updated_at,omitempty"`
+	ID         int64     `json:"id,omitempty"`
+	Name       string    `json:"name"`
+	TemplateID int64     `json:"template_id,omitempty"`
+	Records    []*Record `json:"records,omitempty"`
+	CreatedAt  time.Time `json:"created_at,omitempty"`
+	UpdatedAt  time.Time `json:"updated_at,omitempty"`
 }
