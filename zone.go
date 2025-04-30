1c5d041d88629ac2ee9c9b8a4d5d0278937cdc33
Vitalie Cherpec <vitalie@penguin.ro>
2025-04-30 14:31:50 +0300

    Add tags field to zone

--- zone.go
+++ zone.go
@@ -5,6 +5,7 @@ import "time"
 type Zone struct {
 	ID         int64     `json:"id,omitempty"`
 	Name       string    `json:"name"`
+	Tags       []string  `json:"tags,omitempty"`
 	TemplateID int64     `json:"template_id,omitempty"`
 	Records    []*Record `json:"records,omitempty"`
 	CreatedAt  time.Time `json:"created_at,omitempty"`
